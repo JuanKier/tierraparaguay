3d199e36a1815ceb16bbb15a147c024c8457737a
Tu Nombre <juanandreskl1999@gmail.com>
2026-05-18 23:16:12 -0300

    Fix TDZ: convert all export const arrow functions to export async function declarations in api.js

--- src/db/api.js
+++ src/db/api.js
@@ -22,42 +22,36 @@ export async function clearDatabase() {
   localStorage.clear();
 }
 
-// Users
-export const getAllUsers = () => req('GET', '/users');
-export const getUserByUsername = (username) => req('GET', '/users/' + encodeURIComponent(username));
-export const addUser = (data) => req('POST', '/users', data);
-export const updateUser = (id, data) => req('PUT', '/users/' + id, data);
-export const deleteUser = (id) => req('DELETE', '/users/' + id);
+export async function getAllUsers() { return req('GET', '/users'); }
+export async function getUserByUsername(username) { return req('GET', '/users/' + encodeURIComponent(username)); }
+export async function addUser(data) { return req('POST', '/users', data); }
+export async function updateUser(id, data) { return req('PUT', '/users/' + id, data); }
+export async function deleteUser(id) { return req('DELETE', '/users/' + id); }
 
-// Empresas
-export const getAllEmpresas = () => req('GET', '/empresas');
-export const getEmpresaById = (id) => req('GET', '/empresas/' + id);
-export const addEmpresa = (data) => req('POST', '/empresas', data);
-export const updateEmpresa = (id, data) => req('PUT', '/empresas/' + id, data);
-export const deleteEmpresa = (id) => req('DELETE', '/empresas/' + id);
+export async function getAllEmpresas() { return req('GET', '/empresas'); }
+export async function getEmpresaById(id) { return req('GET', '/empresas/' + id); }
+export async function addEmpresa(data) { return req('POST', '/empresas', data); }
+export async function updateEmpresa(id, data) { return req('PUT', '/empresas/' + id, data); }
+export async function deleteEmpresa(id) { return req('DELETE', '/empresas/' + id); }
 
-// Vehiculos
-export const getAllVehiculos = () => req('GET', '/vehiculos');
-export const getVehiculoById = (id) => req('GET', '/vehiculos/' + id);
-export const addVehiculo = (data) => req('POST', '/vehiculos', data);
-export const updateVehiculo = (id, data) => req('PUT', '/vehiculos/' + id, data);
-export const deleteVehiculo = (id) => req('DELETE', '/vehiculos/' + id);
+export async function getAllVehiculos() { return req('GET', '/vehiculos'); }
+export async function getVehiculoById(id) { return req('GET', '/vehiculos/' + id); }
+export async function addVehiculo(data) { return req('POST', '/vehiculos', data); }
+export async function updateVehiculo(id, data) { return req('PUT', '/vehiculos/' + id, data); }
+export async function deleteVehiculo(id) { return req('DELETE', '/vehiculos/' + id); }
 
-// Mercaderias
-export const getAllMercaderias = () => req('GET', '/mercaderias');
-export const addMercaderia = (data) => req('POST', '/mercaderias', data);
-export const updateMercaderia = (id, data) => req('PUT', '/mercaderias/' + id, data);
-export const deleteMercaderia = (id) => req('DELETE', '/mercaderias/' + id);
+export async function getAllMercaderias() { return req('GET', '/mercaderias'); }
+export async function addMercaderia(data) { return req('POST', '/mercaderias', data); }
+export async function updateMercaderia(id, data) { return req('PUT', '/mercaderias/' + id, data); }
+export async function deleteMercaderia(id) { return req('DELETE', '/mercaderias/' + id); }
 
-// Boletas
-export const getAllBoletas = () => req('GET', '/boletas');
-export const getBoletaById = (id) => req('GET', '/boletas/' + id);
-export const addBoleta = (data) => req('POST', '/boletas', data);
-export const updateBoleta = (id, data) => req('PUT', '/boletas/' + id, data);
-export const deleteBoleta = (id) => req('DELETE', '/boletas/' + id);
+export async function getAllBoletas() { return req('GET', '/boletas'); }
+export async function getBoletaById(id) { return req('GET', '/boletas/' + id); }
+export async function addBoleta(data) { return req('POST', '/boletas', data); }
+export async function updateBoleta(id, data) { return req('PUT', '/boletas/' + id, data); }
+export async function deleteBoleta(id) { return req('DELETE', '/boletas/' + id); }
 
-// Config
-export const getConfig = () => req('GET', '/config');
-export const updateConfig = (data) => req('POST', '/config', data);
-export const exportDatabase = () => req('GET', '/export');
-export const importDatabase = (data) => req('POST', '/import', data);
+export async function getConfig() { return req('GET', '/config'); }
+export async function updateConfig(data) { return req('POST', '/config', data); }
+export async function exportDatabase() { return req('GET', '/export'); }
+export async function importDatabase(data) { return req('POST', '/import', data); }
